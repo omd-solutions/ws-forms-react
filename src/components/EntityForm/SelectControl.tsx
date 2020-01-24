481f1b77a9e0ba2a5097a453ab2c462c387995e9
oliverdennis <oliver.dennis@omdsolutions.co.uk>
2020-01-24 11:42:32 +0000

    Initial import

--- src/components/EntityForm/SelectControl.tsx
+++ src/components/EntityForm/SelectControl.tsx
@@ -6,6 +6,7 @@ import FormControl from "./FormControl";
 class SelectControl implements FormControl<any> {
 
     render(field: FormField, value: any, valueSetter: (value: any) => void): ReactNode {
+        let values = field.values ? field.values : [];
         return (
             <MatFormControl fullWidth>
                 <InputLabel id={'control-label-' + field.fieldName}>{field.caption}</InputLabel>
@@ -13,7 +14,7 @@ class SelectControl implements FormControl<any> {
                         labelId={'control-label-' + field.fieldName}
                         value={value}
                         onChange={event => valueSetter(event.target.value)}>
-                    {field.values.map(val => (
+                    {values.map(val => (
                         <MenuItem value={val}>
                             {field.displayField ? val[field.displayField] : val}
                         </MenuItem>
